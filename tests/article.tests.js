9afed87217371b44c015ef5115039d96c73cb4bb
Eli250 <hirwaeli@outlook.com>
2022-02-04 17:02:21 +0200

    Final Tests

--- tests/article.tests.js
+++ tests/article.tests.js
@@ -1,38 +1,47 @@
 import request from "supertest";
 import { expect, use } from "chai";
 import chaiHttp from "chai-http";
+import { before } from "mocha";
 import app from "../src/app";
-import Article from "../src/models/article";
 import "dotenv/config";
 
 use(chaiHttp);
 
 const testArticle = {
   title: "This is a testing article!",
-  description: "This checks if we are able to create the new article.",
-  content: "This is the created article.",
-  image:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1175px-Test-Logo.svg.png",
+  content: "This checks if we are able to create the new article.",
+  description: "This is the created article.",
 };
 
+let tempToken = "";
 describe("ARTICLE END-POINT TESTING", () => {
-  it("Should retrieve the articles", async () => {
-    const res = await request(app).get("/api/v1/articles");
-    expect(res).to.have.status([200]);
-    expect(res.type).to.have.equal("application/json");
+  before("Log In First", (done) => {
+    request(app)
+      .post("/api/v1/user/login")
+      .send({
+        email: "hirwa7@test.com",
+        password: "@Password1",
+      })
+      .expect(200)
+      .then((res) => {
+        expect(res.body.message).to.be.eql("Successfully Logged In!");
+        tempToken = `Bearer ${res.body.accessToken}`;
+        done();
+      })
+      .catch((err) => done(err));
   });
-  it("Should retrieve one article", async () => {
-    const res = await request(app).get(
-      "/api/v1/articles/61f42201beffd6aaf5a96956"
-    );
-    expect(res).to.have.status([200]);
-    // expect(res.type).to.have.equal("application/json");
+  it("Should Allow Create Article.", (done) => {
+    request(app)
+      .post("/api/v1/articles")
+      .set("Authorization", tempToken)
+      .attach("image", "./public/Victor Status.png", "status.png")
+      .field(testArticle)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
   });
-  it("Should not retrieve the articles", async () => {
-    const res = await request(app).get("/api/v1/aritcle/");
-    expect(res).to.have.status([404]);
-  });
-  it("Should not allow anauthorized access to the article", (done) => {
+  it("Should Not Allow Create Article.", (done) => {
     request(app)
       .post("/api/v1/articles")
       .send(testArticle)
@@ -42,5 +51,20 @@ describe("ARTICLE END-POINT TESTING", () => {
         done();
       })
       .catch((err) => done(err));
+  });
+  it("Should Get All Articles", async () => {
+    const res = await request(app).get("/api/v1/articles");
+    expect(res).to.have.status([200]);
+    expect(res.type).to.have.equal("application/json");
+  });
+  it("Should Get One Article", async () => {
+    const res = await request(app).get(
+      "/api/v1/articles/61f42201beffd6aaf5a96956"
+    );
+    expect(res).to.have.status([200]);
+  });
+  it("Should Not Get Any Article", async () => {
+    const res = await request(app).get("/api/v1/aritcle/");
+    expect(res).to.have.status([404]);
   });
 });
